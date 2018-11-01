9b8f04be21e7d22a875c62d8f2c9a3cab7f72f0a
Kyle Combs <combskyle@gmail.com>
2018-11-01 15:42:36 -0400

    webpack config and boilerplate completed

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDir = 'dist';
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|ttf|svg|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 8192 }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDir]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // favicon: './public/favicon.png'
+    })
+  ]
+}
