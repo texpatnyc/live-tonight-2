5c255b08585be10e2bb8c8a7674d35d2717a69b2
Kyle Combs <combskyle@gmail.com>
2018-11-01 15:19:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      user: 'Kyle'
+    }
+
+    //bind functions
+
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div>
+        Hello, {this.state.user}!  Welcome to your new App!
+      </div>
+    )
+  }
+
+}
